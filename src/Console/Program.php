bd5f6ca371f576100320a6bb37a5dc889f63fef1
tothn <norbert.toth@innonic.com>
2021-02-26 11:46:54 +0100

    start

---
+++ src/Console/Program.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Console;
+
+class Program
+{
+    /**
+     * @param string[] $args
+     * @return void
+     */
+    public static function main(array $args)
+    {
+    }
+}
