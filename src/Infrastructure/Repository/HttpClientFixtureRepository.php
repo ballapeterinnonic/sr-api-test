dc1a96dcb05882dd7dd534672dc02445325e5a5c
tothn <norbert.toth@innonic.com>
2021-02-26 17:29:15 +0100

    HttpClientFixtureRepository.php and FilesystemFixtureRepository.php moved to Repository folder

--- src/Infrastructure/Repository/HttpClientFixtureRepository.php
+++ src/Infrastructure/Repository/HttpClientFixtureRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Infrastructure\Fixture;
+namespace App\Infrastructure\Repository;
 
 use App\Domain\FixtureRepositoryInterface;
 use App\Infrastructure\HttpClient\HttpClientInterface;
