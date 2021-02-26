165572f89129a08d4db3bf22816f74be846e1b72
tothn <norbert.toth@innonic.com>
2021-02-26 18:05:27 +0100

    create dir in FilesystemResultRepository.php

--- src/Infrastructure/Repository/FilesystemResultRepository.php
+++ src/Infrastructure/Repository/FilesystemResultRepository.php
@@ -3,7 +3,12 @@
 namespace App\Infrastructure\Repository;
 
 use App\Domain\ResultRepositoryInterface;
+use RuntimeException;
+use function dirname;
+use function file_exists;
+use function is_dir;
 use function json_encode;
+use function mkdir;
 use function sprintf;
 use function strtolower;
 use const JSON_PRETTY_PRINT;
@@ -37,6 +42,23 @@ class FilesystemResultRepository implements ResultRepositoryInterface
 
         $file = sprintf('%s/%s/%s.json', $this->path, $name, $method);
 
+        $dir = dirname($file);
+
+        if (!file_exists($dir)) {
+            $this->createDir($dir);
+        }
+
         file_put_contents($file, json_encode($payload, JSON_PRETTY_PRINT));
+    }
+
+    /**
+     * @param string $dir
+     * @return void
+     */
+    private function createDir(string $dir)
+    {
+        if (!mkdir($dir, 0777, true) && !is_dir($dir)) {
+            throw new RuntimeException(sprintf('Directory "%s" was not created', $dir));
+        }
     }
 }
